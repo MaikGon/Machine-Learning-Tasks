2b59d495e92863e34f831b5cdfffc5025a39d114
MaikGon <michal.gontarczyk@wp.pl>
2020-11-21 16:46:37 +0100

    Added few tasks

--- lab3.py
+++ lab3.py
@@ -1,13 +1,15 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-from sklearn import svm
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn import preprocessing
 from matplotlib.colors import ListedColormap
 from mlxtend.plotting import plot_decision_regions
 from yellowbrick.contrib.classifier import DecisionViz
+from sklearn import svm, tree, ensemble, linear_model
+from sklearn import metrics
+from sklearn import model_selection
 
 
 def lab3_1():
@@ -57,7 +59,7 @@ def lab3_1_1():
     data = datasets.load_iris()
     X, y = data.data, data.target
 
-    print(np.bincount(y)) # ilosc binow (cech)
+    print(np.bincount(y))  # ilosc binow (cech)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                         train_size=0.8, random_state=42, stratify=y)
@@ -95,6 +97,70 @@ def lab3_1_1():
     plt.show()
 
 
+def lab3_2():
+    data = datasets.load_iris()
+    X, y = data.data, data.target
+    X = X[:, [0, 2]]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.75, random_state=42, stratify=y)
+    min_max_scaler = preprocessing.MinMaxScaler()
+    min_max_scaler.fit(X_train)
+
+    X_train = min_max_scaler.transform(X_train)
+    X_test = min_max_scaler.transform(X_test)
+
+    svm_clf = svm.SVC(kernel='poly')
+    svm_clf.fit(X_train, y_train)
+    acc_svm = metrics.accuracy_score(y_test, svm_clf.predict(X_test))
+    print("SVM accuracy: ", acc_svm)
+
+    linear_clf = linear_model.LogisticRegression()
+    linear_clf.fit(X_train, y_train)
+    acc_lin = metrics.accuracy_score(y_test, linear_clf.predict(X_test))
+    print("Logistic regression accuracy: ", acc_lin)
+
+    tree_clf = tree.DecisionTreeClassifier(max_depth=5)
+    tree_clf.fit(X_train, y_train)
+    acc_tree = metrics.accuracy_score(y_test, tree_clf.predict(X_test))
+    print("Decision tree accuracy: ", acc_tree)
+
+    forest_clf = ensemble.RandomForestClassifier(n_estimators=500)
+    forest_clf.fit(X_train, y_train)
+    acc_for = metrics.accuracy_score(y_test, forest_clf.predict(X_test))
+    print("Random forest accuracy: ", acc_for)
+
+    # Now search for best hyperparameters
+    param_grid = [
+        {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+        {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']}
+    ]
+
+    clf_gs = model_selection.GridSearchCV(estimator=svm.SVC(), param_grid=param_grid, n_jobs=4, verbose=20)
+    clf_gs.fit(X_train, y_train)
+    print(clf_gs.cv_results_)
+
+    print("SVM: ", svm_clf.predict(min_max_scaler.transform([[8.0, 4.0]])))
+    print("Linear: ", linear_clf.predict(min_max_scaler.transform([[8.0, 4.0]])))
+    print("Decision tree: ", tree_clf.predict(min_max_scaler.transform([[8.0, 4.0]])))
+    print("Random forest: ", forest_clf.predict(min_max_scaler.transform([[8.0, 4.0]])))
+
+    plt.figure()
+    plot_decision_regions(X_train, y_train, clf=svm_clf, legend=2)
+    plt.figure()
+    plot_decision_regions(X_train, y_train, clf=linear_clf, legend=2)
+    plt.figure()
+    plot_decision_regions(X_train, y_train, clf=tree_clf, legend=2)
+    plt.figure()
+    plot_decision_regions(X_train, y_train, clf=forest_clf, legend=2)
+    plt.show()
+
+
+def lab3_3():
+    pass
+
+
 if __name__ == "__main__":
     # lab3_1()
-    lab3_1_1()
+    # lab3_1_1()
+    lab3_2()
+    # lab3_3()
